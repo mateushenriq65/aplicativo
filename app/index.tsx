93b7c74f05bcbc6f8dd3a11fdbe4a00b7ecd96bb
mateus <theusmhff@gmail.com>
2025-09-18 21:02:40 -0300

    primeira tela pronta

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,49 @@
 
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+
+     const handlePress = () => {    //aqui estou declarando o botão 1
+    console.log('Botão clicado!');    
+  };  
+
+  const handlePress2 = () => {         //aqui estou declarando o botão 2 
+    console.log('Botão 2 clicado!');
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Image
+        <View style={{ flex: 1}}>
+        <View style={styles.container}>
+        <Image                                       
         source={require('../assets/logoo.png')}
-      />
+        />
       
       <Text style={styles.texto1}>Discover Your Dream job here</Text>
       <Text style={styles.subtext}>Explore all the existing job based on your interest and study major</Text>
     </View>
-  )
-}
-    <View
-      style={styles.container2}>
+  
+     
+      <View style={styles.container2}>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
 
        
+          {/* Botão 2 */}
+        <TouchableOpacity style={styles.button2} onPress={handlePress2}>
+          <Text style={styles.buttonText2}>Register</Text>
+        </TouchableOpacity>
 
-
-
-     
-
+      </View>
 
     </View>
-
+  );
+}
 
     
-   
 
-   
-  
 const styles = StyleSheet.create({
   container: {
     
@@ -47,6 +59,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex:1,
     gap:16,
+  
   },
 
   imagem: {
@@ -73,9 +86,33 @@ const styles = StyleSheet.create({
     fontSize: 20,  
     margin:20,
   },
-     
 
+  button: {
+    backgroundColor: '#1F41BB',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
 
+button2: {
+    backgroundColor: '#ffffffff',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+},
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+
+  },
+    
+    buttonText2: {
+    color: '#000000ff',
+    fontSize: 16,
+    fontWeight: 'bold'
+    },
 
 });
 
